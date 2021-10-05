14a954355abbd21329cda1c41b1b31445aebe26c
msb2156 <msb2156@gmail.com>
2021-10-05 01:25:44 -0400

    +routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css'
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
 
   
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+    </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
-
-    <App />
+<BrowserRouter>
+  <App />
+</BrowserRouter>
  ,
   document.getElementById('root')
 );
