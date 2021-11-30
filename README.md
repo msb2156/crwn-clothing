c3943fc54d55b754184c9f4a555eacf0ce989d70
msb2156 <msb2156@gmail.com>
2021-11-30 00:22:16 -0500

    fix

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Practicing on styling
+npm create-react-app crwn-clothing
+npm install node-sass
+
+UPGRADE steps:
+remove .lock file
+npm install
+npm audit fix
+npm update -D  // update dependencies
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -18,8 +18,8 @@ const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
 
+        </div>
     </div>
-</div>
 );
 }
 
